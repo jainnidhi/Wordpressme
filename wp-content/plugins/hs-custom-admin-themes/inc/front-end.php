f0f432dc8f5c479abc5fff3f64489b8cb5d80215
Nidarshana <sharma.nidarshana@gmail.com>
2014-07-14 12:59:20 +0530

    test commit

--- wp-content/plugins/hs-custom-admin-themes/inc/front-end.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-$hs_admin_theme_plugin_url = WP_PLUGIN_URL . '/hs-custom-admin-themes';
-echo '<link rel="stylesheet" type="text/css" href="' . $hs_admin_theme_plugin_url . '/inc/css/custom.css" />';
-?>
